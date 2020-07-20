9d778bef15b22bfd1b802f1cb60e91b318f98e74
Thor40 <nicholas.kosik@gmail.com>
2020-07-19 20:09:33 -0500

    verfication

--- models/Comment.js
+++ models/Comment.js
@@ -3,37 +3,40 @@ const moment = require('moment');
 
 const ReplySchema = new Schema(
     {
-        // set custom id to avoid confusion with parent comment _id
-        replyId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        replyBody: {
-            type: String
-        },
-        writtenBy: {
-            type: String
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
-        }
+      // set custom id to avoid confusion with parent comment _id
+      replyId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      replyBody: {
+        type: String,
+        trim: true,
+        required: true
+      },
+      writtenBy: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      }
     },
     {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        }
+      toJSON: {
+        getters: true
+      }
     }
-);
+  );
 
 const CommentSchema = new Schema({
     writtenBy: {
         type: String
     },
     commentBody: {
-        type: String
+        type: String,
+        required: true
     },
     createAt: {
         type: Date,
